09d66879d20bc89ae74d70ce18c233f2690d3de8
Chris Tran <chris@tran.pw>
2015-08-15 02:13:41 -0700

    i made it do things

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, redirect
 import twilio.twiml
 
 app = Flask(__name__)
@@ -7,9 +7,21 @@ app = Flask(__name__)
 def hello_monkey():
     """Respond to incoming requests"""
     resp = twilio.twiml.Response()
-    resp.say("Hello Monkey")
+    
+    with resp.gather(numDigits=1, action="/handle-key", method="POST") as g:
+    	g.say("press 1 or something")
 
     return str(resp)
 
+@app.route("/handle-key", methods=['GET', 'POST'])
+def handle_key():
+	digit_pressed = request.values.get('Digits', None)
+	if digit_pressed == "1":
+		resp = twilio.twiml.Response()
+		resp.redirect("http://b9ff5a36.ngrok.io/twl/modified.xml")
+
+		return str(resp)
+	else:
+		return redirect("/")
 if __name__ == "__main__":
     app.run(debug=True)
