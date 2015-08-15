136ff9374331569487ec20c318af332f8720a140
Stefan <stefan@stefanaleksic.com>
2015-08-15 10:51:20 -0700

    Add LITERLALY EVERYTHING

--- run.py
+++ run.py
@@ -1,29 +1,31 @@
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, send_from_directory
 import twilio.twiml
-from tables import *
-import db
+#from tables import *
+# import db
 import urllib
 import os
 from pydub import AudioSegment
+import uuid
+
 
 app = Flask(__name__)
-db = db.DB()
+# db = db.DB()
 
 @app.route("/", methods=['GET', 'POST'])
 def root():
     """Respond to incoming requests"""
     resp = twilio.twiml.Response()
     print request.form.get('CallSid')
-    callid = db.addEntry(Calls(request.form.get('CallSid'), request.form.get('From'), str(list())))
-    print callid
+#     callid = db.addEntry(Calls(request.form.get('CallSid'), request.form.get('From'), str(list())))
+    #print callid
     with resp.gather(numDigits=1, action="/handle-key", method="POST") as g:
-        g.say("Press 1 to beep beep")
+        g.say("Press anything to beep beep")
 
     return str(resp)
 
 def _overlayAllSounds(manySounds):
     #If there are no sounds there it should crash lol
-    combinedsound = manySounds[0] 
+    combinedsound = manySounds[0]
     for sound in manySounds[1:]:
         combinedsound = sound.overlay(combinedsound)
     return combinedsound
@@ -41,29 +43,52 @@ def getSoundFromManySounds(manySoundUrls):
     overlayedsounds = _overlayAllSounds(audiosegments)
     return overlayedsounds
 
+@app.route('/sounds/metronome.mp3')
+def send_metronome():
+    print "yolosweg"
+    return send_from_directory("sounds", "120bpm_click_track.mp3")
+
+@app.route("/sounds/out/<f>")
+def send_result(f):
+    return send_from_directory("sounds/out", f)
+
 @app.route("/handle-key", methods=['GET', 'POST'])
 def handle_key():
     digit_pressed = request.values.get('Digits', None)
-    if digit_pressed == "1":
+
+    if "*" not in digit_pressed:
         resp = twilio.twiml.Response()
-        # resp.play("http://demo.twilio.com/hellomonkey/monkey.mp3")
-        resp.say("beep")
-        resp.record(finishOnKey="*", maxLength="30", action="/handle-recording")
+        resp.record(maxLength="4", action="/handle-recording/" + digit_pressed)
         resp.addRedirect("/")
         return str(resp)
     else:
-        return redirect("/")
+        for dirpath, dnames, fnames in os.walk('sounds/tracks'):
+            for f in fnames:
+                os.remove(os.path.join(dirpath, f))
+        resp = twilio.twiml.Response()
+        resp.addRedirect("/")
+        return str(resp)
 
-@app.route("/handle-recording", methods=['GET', 'POST'])
-def handle_recording():
 
+@app.route("/handle-recording/<int:number>", methods=['GET', 'POST'])
+def handle_recording(number):
     recording_url = request.values.get("RecordingUrl", None)
+    print recording_url
+    urllib.urlretrieve(recording_url, filename="sounds/tracks/" +  str(number) + ".wav")
     resp = twilio.twiml.Response()
     callid = request.form.get('CallSid')
     print callid
-    db.updateEntry(Calls, callid, {'recordings': recording_url})
-    resp.say("this is your tones")
-    resp.play(recording_url)
+    #     db.updateEntry(Calls, callid, {'recordings': recording_url})
+    someSounds = []
+    for dirpath, dnames, fnames in os.walk('sounds/tracks'):
+        for f in fnames:
+            someSounds.append(AudioSegment.from_wav(os.path.join(dirpath, f)))
+    overlayedSound = _overlayAllSounds(someSounds)
+
+    filename = str(uuid.uuid4()) + ".mp3"
+    file_handle = overlayedSound.export("./sounds/out/" + filename, format="mp3")
+
+    resp.play("/sounds/out/" + filename)
     resp.addRedirect("/")
     print recording_url
     return str(resp)
