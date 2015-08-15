ef3e93daea5444c399a674e06d073d112956748d
Zach Latta <zach@zachlatta.com>
2015-08-15 01:06:24 -0700

    Set up Flask server.

---
+++ run.py
@@ -0,0 +1,15 @@
+from flask import Flask
+import twilio.twiml
+
+app = Flask(__name__)
+
+@app.route("/", methods=['GET', 'POST'])
+def hello_monkey():
+    """Respond to incoming requests"""
+    resp = twilio.twiml.Response()
+    resp.say("Hello Monkey")
+
+    return str(resp)
+
+if __name__ == "__main__":
+    app.run(debug=True)
